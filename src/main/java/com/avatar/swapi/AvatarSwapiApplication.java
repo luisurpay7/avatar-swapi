7dbe757b1902a552dd55e860301a4caa7ef89b5d
luisyet <sulcaluis151999@gmail.com>
2023-09-12 15:40:46 -0500

    init commit

---
+++ src/main/java/com/avatar/swapi/AvatarSwapiApplication.java
@@ -0,0 +1,13 @@
+package com.avatar.swapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvatarSwapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvatarSwapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avatar/swapi/AvatarSwapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avatar.swapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvatarSwapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
